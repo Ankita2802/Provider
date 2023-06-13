285c99d97860f476527973285702722d0d68f68a
ankita <ankitavaghela028@gmail.com>
2023-06-13 15:06:48 +0530

    commit all local files

--- lib/stateful_example.dart
+++ lib/stateful_example.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 class Staful_Example extends StatefulWidget {
   const Staful_Example({Key? key}) : super(key: key);
@@ -8,9 +10,21 @@ class Staful_Example extends StatefulWidget {
 
 class _Staful_ExampleState extends State<Staful_Example> {
   @override
-  int x=0;
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer.periodic(Duration(seconds: 01), (timer) {
+      count++;
+      // print(count);
+      setState(() {
+
+      });
+    });
+  }
+  @override
+  int count=0;
   Widget build(BuildContext context) {
-    print("build");
+    print("build"+count.toString());
     return Scaffold(
       appBar: AppBar(
         title: Text("Stateful Exaample"),
@@ -21,15 +35,16 @@ class _Staful_ExampleState extends State<Staful_Example> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          Text(DateTime.now().hour.toString()+":"+DateTime.now().minute.toString()+":"+DateTime.now().second.toString()),
           Container(
-            child:Center(child: Text(x.toString(),style: TextStyle(color: Colors.red,fontSize: 50),),),
+            child:Center(child: Text(count.toString(),style: TextStyle(color: Colors.red,fontSize: 50),),),
           )
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          x++;
-          print(x++);
+          count++;
+          print(count++);
           setState(() {
 
           });
